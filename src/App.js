4bf07519efd107c946580f226cd6953e3a8f6007
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-18 16:43:11 +0700

    Product card component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './components/Header'
 import styled, { ThemeProvider } from 'styled-components'
 import * as theme from './styled/theme'
+import ProductCard from './components/ProductCard'
+import { useSelector } from 'react-redux'
 
 const Container = styled.div`
   display: flex;
@@ -21,6 +23,9 @@ const ProductContainer = styled.div`
   background: ${props => props.theme.light};
   border-left: 1px solid #f7f7f7;
   padding: 0.5rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `
 
 const CartContainer = styled.div`
@@ -29,12 +34,20 @@ const CartContainer = styled.div`
 `
 
 const App = () => {
+  const products = useSelector(state => state.product.products)
   return (
     <ThemeProvider theme={theme}>
       <Header />
       <Container>
         <MenuContainer>Menu</MenuContainer>
-        <ProductContainer>Product</ProductContainer>
+        <ProductContainer>
+          {products.map(product =>
+            <ProductCard
+              key={product.id}
+              item={product}
+            />
+          )}
+        </ProductContainer>
         <CartContainer>Cart</CartContainer>
       </Container>
     </ThemeProvider>
